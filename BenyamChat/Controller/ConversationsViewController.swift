93c2a1f8cb082f021c482050b95551c3032adf59
Raiymbek Merekeyev <raiymbekmerekeyev@Raiymbeks-MacBook-Pro.local>
2022-11-27 19:19:22 +0600

    Initial setup

--- BenyamChat/Controller/ConversationsViewController.swift
+++ BenyamChat/Controller/ConversationsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ConversationsViewController.swift
 //  BenyamChat
 //
 //  Created by Raiymbek Merekeyev on 27.11.2022.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
