3825b3666847015edfa619aaed82738fedd097c1
Raiymbek Merekeyev <raiymbekmerekeyev@Raiymbeks-MacBook-Pro.local>
2022-11-27 19:07:52 +0600

    Start of the project

---
+++ BenyamChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BenyamChat
+//
+//  Created by Raiymbek Merekeyev on 27.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
